b9b8277ca14a7d38b7f1fd182c13ff159a0634e7
Gera Kazakov <gkazakov@msn.com>
2018-08-02 19:46:37 -0600

    Add sdk_config tasks

--- ble_app_blinky/vscode.bat
+++ ble_app_blinky/vscode.bat
@@ -1,5 +1,5 @@
 rem Location of Nordic SDK
-set NRF_SDK=V:/Nordic/nRF5_SDK_15.0.0_a53641a
+set NRF_SDK=V:/Nordic/SDK/nRF5_SDK_15.0.0_a53641a
 
 rem Location of Nordic Command Line tools (nrfjprog) 
 set NRF_TOOLS=C:/Program Files (x86)/Nordic Semiconductor/nrf5x/bin
@@ -12,6 +12,9 @@ set GNU_TOOLS=V:/Nordic/GNU/Tools/2.11-20180428-1604/bin
 
 rem Location of SEGGER JLink tools
 set SEGGER_TOOLS=C:/Program Files (x86)/SEGGER/JLink_V620i
+
+rem Location of java
+set JAVA=C:/Program Files (x86)/Java/jdk1.8.0_161/bin/java.exe
 
 rem Serial numbers of nRF development boards
 set PCA10056_SN=683798348
